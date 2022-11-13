e7adfdc26a9d05ac0c78a264677124ae01ca7978
KidusAlemayehu <kidusalemayehu705@gmail.com>
2022-11-13 14:57:56 +0300

    compression and extraction file almost done

--- file/compression.py
+++ file/compression.py
@@ -1,16 +1,45 @@
 import zipfile
-from django.conf import settings
+import pathlib
+import os
 
-
-def compress_file(filename):
-    with zipfile.ZipFile(filename, 'w') as zf:
-        zf.write(filename, compress_type=zipfile.ZIP_DEFLATED)
+def compress_file(path):
+    file_path = path
+    filename = open(path, 'r')
+    fpath = pathlib.Path(file_path)
+    zf_path = "".join([fpath.stem,".zip"])
+    zf_path=os.path.join(fpath.parent,zf_path)
+    print(zf_path)
+    with zipfile.ZipFile(zf_path, mode='w') as zf:
+        dir, base_filename = os.path.split(path)
+        os.chdir(dir)
+        zf.write(base_filename, compress_type=zipfile.ZIP_BZIP2)
+    # print(zf.getinfo)
     zf.close()
     zf.printdir()
-    # print(zf)
+    if os.path.exists(path):
+        os.remove(path)
+    else:
+        print("Can not delete the file as it doesn't exists")
+    print("___________________")
+    print(zf.filename)
+    print("___________________")
 
 def decompress_file(filename, original_name):
-    with zipfile.Zipfile(filename, 'r') as zf:
+    file_path = filename
+    fpath = pathlib.Path(file_path)
+    zf_path = "".join([fpath.stem,".zip"])
+    zf_path=os.path.join(fpath.parent,zf_path)
+    with zipfile.ZipFile(zf_path, 'r') as zf:
+        dir, base_filename = os.path.split(filename)
+        os.chdir(dir)
         zf.extract(original_name)
     zf.close()
-    # return zf
+    zf.printdir()
+    if os.path.exists(zf_path):
+        os.remove(zf_path)
+    else:
+        print("Can not delete the file as it doesn't exists")
+    print("___________________")
+    print(zf.filename)
+    print("___________________")
+
