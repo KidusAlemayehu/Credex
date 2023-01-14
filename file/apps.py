7e0579f2dc9624298a37aa06aec3a658043121bb
KidusAlemayehu <kidusalemayehu705@gmail.com>
2023-01-14 11:20:29 +0300

    requirments added

--- file/apps.py
+++ file/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
-
+from django.core.signals import request_finished
+from . import signals
 
 class FileConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'file'
+
+    def ready(self):
+        request_finished.connect(signals.my_callback)
